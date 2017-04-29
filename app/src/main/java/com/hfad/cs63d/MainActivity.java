0f8dc2f2826999c07175d52e0c8f21569e69f023
Roberto Amparan <mr.amparan@gmail.com>
2017-04-29 15:32:02 -0700

    Revert "meme once again"

--- app/src/main/java/com/hfad/cs63d/MainActivity.java
+++ app/src/main/java/com/hfad/cs63d/MainActivity.java
@@ -7,7 +7,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -20,8 +19,6 @@ public class MainActivity extends AppCompatActivity {
         private TextView textHistory;
     private TextView textFavorites;
     private TextView textAZ;
-    ViewPager viewPager;
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,11 +28,6 @@ public class MainActivity extends AppCompatActivity {
         textFavorites = (TextView) findViewById(R.id.text_favorites);
         textAZ = (TextView) findViewById(R.id.text_az);
 
-        viewPager = (ViewPager) findViewById(R.id.viewPager);
-
-        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(this);
-
-        viewPager.setAdapter(viewPagerAdapter);
 
         BottomNavigationView bottomNavigationView = (BottomNavigationView)
                 findViewById(R.id.bottom_navigation);
